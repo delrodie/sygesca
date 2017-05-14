5c31ec2bcf05eb229b154ab6e2b917e6ea6e17fb
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-05-14 17:29:13 +0000

    Mise en page des pages de connexion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,29 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+      $session = $request->getSession();
+        $userIp = $request->getClientIp();
+
+        $user = $this->getUser();
+
+      $date = new \DateTime();
+        //$datet = new \DateTimeZone('Europe/Paris');
+
+        // Envoie de mail de notification à l'administrateur
+        $message = \Swift_Message::newInstance()
+                  ->setSubject('ARFISGED Notifications')
+                  ->setFrom('noreply@arfis.com')
+                  ->setTo('delrodieamoikon@gmail.com')
+                  ->setBody(
+                  $this->renderView('emails/connexion.html.twig', array(
+                      'user' => $user,
+                      'ip' => $userIp,
+                      'date_connexion'  => $date->format('D d M Y h:i:s'),
+                    )),
+                      'text/html'
+                  );
+        $this->get('mailer')->send($message);
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
