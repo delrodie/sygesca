1d9052cde79c6e926da0cfb90d3c977499b3aca1
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-05-15 06:56:11 +0000

    Mise en page de la clase User

---
+++ web/ressources/js/sygesca-form-validation.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    var a = {
+        valid: "fa fa-check-circle fa-lg text-success",
+        invalid: "fa fa-times-circle fa-lg",
+        validating: "fa fa-refresh"
+    };
+    $("#userForm").bootstrapValidator({
+        framework: "bootstrap",
+        icon: {
+            valid: "glyphicon glyphicon-ok",
+            invalid: "glyphicon glyphicon-remove",
+            validating: "glyphicon glyphicon-refresh"
+        },
+        fields: {
+            'appbundle_user[username]': {
+                validators: {
+                    notEmpty: { message: "Le nom utilisateur est obligatoire." },
+                    stringLength: { min: 5, max: 15, message: "Ce champ doit avoir au moins 5 caractères" },
+                    regexp: { regexp: /^[a-zA-Z0-9_\.]+$/, message: "Le nom utilisateur doit être uniquement alphanumerique. il ne doit pas comportement de tiret ni d'espace" }
+                }
+            },
+            'appbundle_user[email]': {
+                validators: {
+                    notEmpty: { message: "L'adresse mail est obligatoire." },
+                    emailAddress: { message: "Cette adresse email n'est pas valide" }
+                }
+            },
+            'appbundle_user[password]': {
+                validators: {
+                    notEmpty: { message: "Le mot de passe est obligatoire" },
+                    different: { field: "appbundle_user[username]", message: "Le mot de passe ne doit pas être idenitique au nom utilisateur." }
+                }
+            },
+            dateNaiss: {
+                validators: {
+                    notEmpty: { message: "La date de naissance doit être obligatoire" },
+                    //stringLength: { min: 6, max: 30, message: "The username must be more than 6 and less than 30 characters long" },
+                    regexp: { regexp: /^[Z0-9-\.]+$/, message: "La date de naissance doit être au format jj-mm-aaaa (ex: 22-02-1941)" }
+                }
+            }
+        }
+    })
+});
